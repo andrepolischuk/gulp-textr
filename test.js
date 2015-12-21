7bbfa781ac8a4050868537f8b5956e3b7875f12d
andrepolischuk <andre.polischuk@gmail.com>
2015-12-21 15:57:00 +0300

    Bump deps

--- test.js
+++ test.js
@@ -3,25 +3,26 @@ import {File} from 'gulp-util';
 import ellipses from 'typographic-ellipses';
 import quotes from 'typographic-quotes';
 import spaces from 'typographic-single-spaces';
-import textr from './index';
+import textr from './index.es5';
 
 const fixture = new File({
   path: 'fixture.txt',
   contents: new Buffer(`Hello  "world"...\n`)
 });
 
-test('should return original text', async t => {
+test.cb('should return original text', t => {
   const stream = textr();
 
   stream.on('data', file => {
     t.is(file.relative, 'fixture.txt');
     t.is(file.contents.toString(), `Hello  "world"...\n`);
+    t.end();
   });
 
   stream.write(fixture);
 });
 
-test('should return transformed text', async t => {
+test.cb('should return transformed text', t => {
   const stream = textr({locale: 'en-us'})
     .use(spaces)
     .use(quotes)
@@ -31,6 +32,7 @@ test('should return transformed text', async t => {
   stream.on('data', file => {
     t.is(file.relative, 'fixture.txt');
     t.is(file.contents.toString(), `Hello “world”…`);
+    t.end();
   });
 
   stream.write(fixture);
